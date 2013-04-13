9dab665183194ec4bd99b34cbd0c45cfd75aadaf
lmw <lmwang@gmail.com>
2013-04-13 12:08:42 -0700

    recovered deleted project files; renamed scrapy project name

---
+++ chapm25/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/topics/items.html
+
+from scrapy.item import Item, Field
+
+class chapmItem(Item):
+    date = Field()
+    time = Field()
+    city = Field()
+    staname=Field()
+    pm25=Field()
+    pm25_index=Field()
+    pm10=Field()
+    pm10_index=Field()
+    # define the fields for your item here like:
+    # name = Field()
+
