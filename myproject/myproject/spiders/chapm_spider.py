b321ba2f64f428bfecb0fb142e4f4a4fb01c4fef
wyaming89 <wyaming89@yahoo.com.cn>
2013-04-13 17:14:03 +0800

    add a new repository

---
+++ myproject/myproject/spiders/chapm_spider.py
@@ -0,0 +1,24 @@
+#coding=gbk
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from mobile.items import chapmItem
+
+class chapm25Spider(BaseSpider):
+    name = "chapm25"
+    allowed_domains = ["chapm25.com"]
+    start_urls = [
+        "http://www.chapm25.com/"
+        
+]    
+  
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        
+        chapm=chapmItem()
+        chapm['staname']=hxs.select('//h2/div/div/a/text()').extract()
+        chapm['pm25']=hxs.select('//table[@class="table table-hover"]/tbody/tr[1]/td/text()').extract()
+        chapm['pm10']=hxs.select('//table[@class="table table-hover"]/tbody/tr[3]/td/text()').extract()
+        chapm['city']=hxs.select('//ul/li[@class="active"]/a/text()').extract()
+        chapm['pm25_index']=hxs.select('//table[@class="table table-hover"]/tbody/tr[2]/td/span/text()').extract()
+        chapm['pm10_index']=hxs.select('//table[@class="table table-hover"]/tbody/tr[4]/td/span/text()').extract()
+        return chapm
