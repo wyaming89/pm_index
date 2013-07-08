ff6123b523747b7e982bcaa16611072153daeea0
wyaming89 <wyaming89@yahoo.com.cn>
2013-07-08 08:35:02 +0800

    以站点分列数据

--- chapm25/spiders/chapm25_spider.py
+++ chapm25/spiders/chapm25_spider.py
@@ -20,58 +20,38 @@ class Chapm25Spider(CrawlSpider):
             callback='parse_item', 
             follow=True),
     )
-    
-    # def parse(self,response):
-        # req=[]
-        # hxs=HtmlXPathSelector(response)
-        # #获取省级单位的链接
-        # pro_url=hxs.select('//div/ul/li[@class="nav-header  "]/a/@href').extract()
-        # for url in pro_url:
-            # r=Request(url,callback=self.parse_city)
-            # req.append(r)
-        # return req
-
-    # def parse_city(self, response):
-        # hxs = HtmlXPathSelector(response)
-        
-        # req=[]
-        # city_urls=[]
-        # citys=['北京','上海','天津','重庆']
-        # province=['福建','广东','河北','江苏','辽宁','山东','浙江']
-        # hxs=HtmlXPathSelector(response)
-        # #获取市级单位的链接
-        # city=hxs.select('//div/ul[@class="nav nav-list"]/li[1]/a/text()').extract()
-        # if city[0] in citys:
-            # urls=hxs.select('//div/ul[@class"nav nav-list"]/li[1]/a/@href').extract()
-            # city_urls.extend(urls)
-        # elif city[0] in province:
-            # urls=hxs.select('//ul[@class="nav nav-list"]/li[not(@class)]/a/@href').extract()
-            # city_urls.extend(urls)
-        # else:
-            # urls=hxs.select('//ul[@class="nav nav-list"]/li[1]/a/@href').extract()
-            # city_urls.extend(urls)
-        # for url in city_urls:
-            
-            # r=Request(url,callback=self.parse_item)
-            # req.append(r)
-        # return req
-        
+       
     def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
-        chapm = chapm25Item()        
-        chapm['staname']=hxs.select('//h2/div/div/a/text()').extract()
-        
-        chapm['pm25_rt'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[1]/td[2]/text()').re('(\d+)')
-        chapm['pm25_24'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[1]/td[3]/text()').re('(\d+)')
-        chapm['pm25_index_rt'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[2]/td[2]/span/text()').re('(\d+)')
-        chapm['pm25_index_24'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[2]/td[3]/span/text()').re('(\d+)')
-        chapm['pm10_rt']=hxs.select('//table[@class="table table-hover"]/tbody/tr[3]/td[2]/text()').re('(\d+)')
-        chapm['pm10_24']=hxs.select('//table[@class="table table-hover"]/tbody/tr[3]/td[3]/text()').re('(\d+)')
-        chapm['pm10_index_rt'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[4]/td[2]/span/text()').re('(\d+)')
-        chapm['pm10_index_24'] = hxs.select('//table[@class="table table-hover"]/tbody/tr[4]/td[3]/span/text()').re('(\d+)')
-        #chapm['city']=hxs.select('//ul/li[@class="active"]/a/text()').extract()
-        chapm['time'] = hxs.select('//footer').re('(\d+:\d+:\d+)')
-        chapm['date'] = hxs.select('//footer').re('(\d+-\d+-\d+)')
-        chapm['city']=hxs.select('//title').re('(\w+)PM2.5')
-        
-        return chapm
+                
+        staname = hxs.select('//h2/div/div/a/text()').extract()
+        pm25_rt = hxs.select('//table[@class="table table-hover"]/tbody/tr[1]/td[2]/text()').re('(\d+)')
+        pm25_24 = hxs.select('//table[@class="table table-hover"]/tbody/tr[1]/td[3]/text()').re('(\d+)')
+        pm25_index_rt = hxs.select('//table[@class="table table-hover"]/tbody/tr[2]/td[2]/span/text()').re('(\d+)')
+        pm25_index_24 = hxs.select('//table[@class="table table-hover"]/tbody/tr[2]/td[3]/span/text()').re('(\d+)')
+        pm10_rt=hxs.select('//table[@class="table table-hover"]/tbody/tr[3]/td[2]/text()').re('(\d+)')
+        pm10_24=hxs.select('//table[@class="table table-hover"]/tbody/tr[3]/td[3]/text()').re('(\d+)')
+        pm10_index_rt = hxs.select('//table[@class="table table-hover"]/tbody/tr[4]/td[2]/span/text()').re('(\d+)')
+        pm10_index_24 = hxs.select('//table[@class="table table-hover"]/tbody/tr[4]/td[3]/span/text()').re('(\d+)')  
+        time = hxs.select('//footer').re('(\d+:\d+:\d+)')
+        date = hxs.select('//footer').re('(\d+-\d+-\d+)')
+        city=hxs.select('//title').re('(\w+)PM2.5')
+        items = []
+        for i  in range(len(staname)):
+            
+            chapm = chapm25Item()
+            chapm['staname'] = staname[i]
+            chapm['pm25_rt'] = pm25_rt[i]
+            chapm['pm25_24'] = pm25_24[i]
+            chapm['pm25_index_rt'] = pm25_index_rt[i]
+            chapm['pm25_index_24'] = pm25_index_24[i]
+            chapm['pm10_rt'] = pm10_rt[i]
+            chapm['pm10_24'] = pm10_24[i]
+            chapm['pm10_index_rt'] = pm10_index_rt[i]
+            chapm['pm10_index_24'] = pm10_index_24[i]
+            
+            chapm['time'] = time
+            chapm['date'] = date
+            chapm['city']=city
+            items.append(chapm)
+        return items
